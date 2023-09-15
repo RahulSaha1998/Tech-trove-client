a844414662514119b659587e0bb87f2a77ab7e11
Rahul Saha <rahulsaha87928@gmail.com>
2023-09-15 12:31:07 +0600

    customize myCart

---
+++ src/components/MyCartTable.jsx
@@ -0,0 +1,25 @@
+
+
+const MyCartTable = ({item, index, handleDelete}) => {
+
+    const { _id, price, quantity, product_name, image, rating } = item;
+
+
+    return (
+        <>
+            <tbody>
+                <tr key={_id} className='font-bold'>
+                    <th className="text-center">{index + 1}</th>
+                    <th className="text-center"><img className="w-24 mx-auto" src={image} alt="" /></th>
+                    <td className="text-center">{product_name}</td>
+                    <td className="text-center">${price}</td>
+                    <td className="text-center">{quantity}</td>
+                    <td className="text-center">{rating}</td>
+                    <td className='text-center'><button onClick={() => handleDelete(_id)} className='btn btn-error'>Delete</button></td>
+                </tr>
+            </tbody>
+        </>
+    );
+};
+
+export default MyCartTable;
--- src/pages/MyCart/MyCart.jsx
+++ src/pages/MyCart/MyCart.jsx
@@ -3,6 +3,7 @@ import { Helmet } from 'react-helmet-async';
 import { Link } from 'react-router-dom';
 import useAxios from '../../hooks/useAxios';
 import toast from 'react-hot-toast';
+import MyCartTable from '../../components/MyCartTable';
 
 
 const MyCart = () => {
@@ -12,13 +13,13 @@ const MyCart = () => {
     const API = useAxios();
 
     // Fetch carted products
-    useEffect(()=>{
+    useEffect(() => {
         API('cartProducts')
-        .then((res) => {
-            setCartProduct(res.data);
-          })
-          .catch((err) => console.error(err));
-      }, []);
+            .then((res) => {
+                setCartProduct(res.data);
+            })
+            .catch((err) => console.error(err));
+    }, []);
 
 
     // Function to handle product deletion
@@ -28,7 +29,7 @@ const MyCart = () => {
             .then(data => {
                 if (data?.data?.deletedCount > 0) {
                     toast.success("Product Deleted successfully!");
-    
+
                     const remaining = cartProduct.filter(product => product._id !== _id);
                     setCartProduct(remaining);
                 }
@@ -42,51 +43,39 @@ const MyCart = () => {
     return (
         <div>
             <Helmet>
-                <title>REPLIQ | Cart</title>
+                <title>TECH TROVE | Cart</title>
             </Helmet>
             <div className="flex items-center gap-5 justify-end mb-5">
                 <p className="text-lg font-semibold">Total: ${total}</p>
-                <Link to="/cart/checkout" state={total}> 
+                <Link to="/cart/checkout" state={cartProduct}>
                     <button className='btn btn-outline'>Checkout</button>
                 </Link>
             </div>
             <div className="overflow-x-auto">
                 <div className="overflow-x-auto card shadow-lg">
 
-                        <table className="table table-zebra w-full">
-                            {/* head */}
-                            <thead>
-                                <tr className='bg-slate-400 text-slate-800'>
-                                    <th className='text-center'>Serial</th>
-                                    <th className='text-center'>Photo</th>
-                                    <th className='text-center'>Name</th>
-                                    <th className='text-center'>Email</th>
-                                    <th className='text-center'>Price</th>
-                                    <th className='text-center'>Quantity</th>
-                                    <th className='text-center'>rating</th>
-                                    <th className='text-center'>Delete</th>
-
-
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    cartProduct.map((product, index) =>
-                                        <tr key={product._id} className='font-bold'>
-                                            <th className="text-center">{index + 1}</th>
-                                            <th className="text-center"><img className="w-24 mx-auto" src={product?.image} alt="" /></th>
-                                            <td className="text-center">{product.product_name}</td>
-                                            <td className="text-center">{product.email}</td>
-                                            <td className="text-center">${product.price}</td>
-                                            <td className="text-center">{product.quantity}</td>
-                                            <td className="text-center">{product.rating}</td>
-                                            <td className='text-center'><button onClick={() => handleDelete(product._id)} className='btn btn-error'>Delete</button></td>
-
-                                        </tr>)
-                                }
-
-                            </tbody>
-                        </table>
+                    <table className="table table-zebra w-full">
+                        {/* head */}
+                        <thead>
+                            <tr className='bg-slate-400 text-slate-800'>
+                                <th className='text-center'>Serial</th>
+                                <th className='text-center'>Photo</th>
+                                <th className='text-center'>Name</th>
+                                <th className='text-center'>Price</th>
+                                <th className='text-center'>Quantity</th>
+                                <th className='text-center'>rating</th>
+                                <th className='text-center'>Delete</th>
+                            </tr>
+                        </thead>
+                        {
+                            cartProduct.map((item, index) => <MyCartTable
+                                index={index}
+                                key={item._id}
+                                item={item}
+                                handleDelete={handleDelete}
+                            ></MyCartTable>)
+                        }
+                    </table>
                 </div>
             </div>
         </div>
